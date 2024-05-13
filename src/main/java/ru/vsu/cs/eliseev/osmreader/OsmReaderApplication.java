8b7a5add974bd8c19005c94e40f8fdf2ae1288bb
Alexander Eliseev <alexelis2003@gmail.com>
2024-05-13 16:35:38 +0300

    project template

---
+++ src/main/java/ru/vsu/cs/eliseev/osmreader/OsmReaderApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.eliseev.osmreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsmReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsmReaderApplication.class, args);
+    }
+
+}
